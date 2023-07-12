ee654e0ce7c0f5c5ef6ac29eaf06915ae56da9d1
Ifechi <j.cyxtus@gmail.com>
2023-07-12 23:53:14 +0100

    Added readme and gorilla mux

--- api.go
+++ api.go
@@ -1,9 +1,21 @@
 package main
 
+import (
+	"net/http"
+)
+
 type ApiServer struct {
 	listenAddr string
 }
 
 func NewApiServer(listenAddr string) *ApiServer {
 	return &ApiServer{listenAddr: listenAddr}
+}
+
+func (server *ApiServer) Run() {
+
+}
+
+func (server *ApiServer) handleAccount(rw *http.ResponseWriter, req *http.Request) error {
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/if3chi/PiggyVault
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
