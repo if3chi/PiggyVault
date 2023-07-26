4ba72d9431d02bc1e4d84e1e183cb9cd2f1ca5e8
Ifechi <j.cyxtus@gmail.com>
2023-07-26 23:55:56 +0100

    WIP

--- api.go
+++ api.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-type ApiServer struct {
-	listenAddr string
+type ApiServer struct{ listenAddr string }
+type apiFunc func(http.ResponseWriter, *http.Request) error
+type ApiError struct{ Error string }
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+
+	return json.NewEncoder(w).Encode(v)
+}
+
+func makeHTTPHandler(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
 }
 
 func NewApiServer(listenAddr string) *ApiServer {
@@ -17,10 +33,10 @@ func NewApiServer(listenAddr string) *ApiServer {
 func (server *ApiServer) Run() {
 	router := mux.NewRouter()
 
-	router.HandleFunc()
+	router.HandleFunc("/account", makeHTTPHandler(server.handleAccount))
 }
 
-func (server *ApiServer) handleAccount(rw *http.ResponseWriter, req *http.Request) error {
+func (server *ApiServer) handleAccount(w *http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
