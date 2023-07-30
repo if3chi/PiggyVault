90b67ae942efb719dc29fd0a9bcae254ae30808d
Ifechi <j.cyxtus@gmail.com>
2023-07-30 23:54:39 +0100

    WIP: Implement Interface

--- db/storage.go
+++ db/storage.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+
 	_ "github.com/lib/pq"
 
 	"github.com/if3chi/PiggyVault/model"
@@ -11,7 +12,7 @@ type Storage interface {
 	CreateAccount(*model.Account) error
 	UpdateAccount(*model.Account) error
 	DeleteAccount(int) error
-	GetAccountByID(int) error
+	GetAccountByID(int) (*model.Account, error)
 }
 
 type PostgresStore struct {
@@ -19,5 +20,32 @@ type PostgresStore struct {
 }
 
 func NewPostgresStore() (*PostgresStore, error) {
+	connStr := "user=postgres dbname=postgres password=piggy sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
 
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &PostgresStore{db: db}, err
+}
+
+func (s *PostgresStore) CreateAccount(*model.Account) error {
+	return nil
+}
+
+func (s *PostgresStore) UpdateAccount(*model.Account) error {
+	return nil
+}
+
+func (s *PostgresStore) GetAccountByID(id int) (*model.Account, error) {
+	return nil, nil
+}
+
+func (s *PostgresStore) DeleteAccount(id int) error {
+	return nil
 }
--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/if3chi/PiggyVault/routes"
+	"github.com/if3chi/PiggyVault/db"
+	// "github.com/if3chi/PiggyVault/routes"
 )
 
 func main() {
 	fmt.Println("PiggyVault")
-	server := routes.NewApiServer(":3000")
+	db, err := db.NewPostgresStore()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
-	server.Run()
+	fmt.Printf("%+v\n", db)
+
+	// server := routes.NewApiServer(":3000", db)
+	// server.Run()
 }
